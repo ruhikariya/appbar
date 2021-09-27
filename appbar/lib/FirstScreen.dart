b7ecfc717c70d739b1fba48f99fdd6f809d668bf
RUCHI <ruchikariya11@gmail.com>
2021-09-27 20:32:20 +0530

    Create FirstScreen.dart

---
+++ appbar/lib/FirstScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';  
+  
+class FirstScreen extends StatelessWidget {  
+  @override  
+  Widget build(BuildContext context) {  
+    return Container(  
+      child: Center(  
+        child: Text('It is a first screen',  
+            style: TextStyle(fontSize: 32.0),  
+          )  
+      ),  
+    );  
+  }  
+} 
