e182196aed215045baa7ff851fa6b5a059c9b5a8
RUCHI <ruchikariya11@gmail.com>
2021-09-27 20:32:08 +0530

    Create SecondScreen.dart

---
+++ appbar/lib/SecondScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';  
+  
+class SecondScreen extends StatelessWidget {  
+  @override  
+  Widget build(BuildContext context) {  
+    return Container(  
+      child: Center(  
+        child: Text('It is a second screen',  
+        style: TextStyle(fontSize: 35.0),  
+        ),  
+      ),  
+    );  
+  }  
+}  
